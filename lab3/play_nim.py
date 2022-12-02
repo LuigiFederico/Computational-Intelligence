065a5750e49552a67c3ffd70f3d34647fc4b5291
Luigi Federico <luigi.fede.22@gmail.com>
2022-12-02 19:40:44 +0100

    Task2 work in progress

--- lab3/play_nim.py
+++ lab3/play_nim.py
@@ -10,7 +10,30 @@ def nim_sum(elem: list):
     x = e ^ x
   return x
 
+##################
+##  STRATEGIES  ##
+##################
 
+# Level 0: Easy
+def dumb_action(nim: Nim):
+  """
+  Always takes one obj from the first row available
+  """
+  row = [r for r, n in enumerate(nim._rows) if n > 0][0]
+  return Nimply(row, 1)
+
+
+# Level 1: Medium
+def dumb_random_action(nim: Nim):
+  """
+  There is 0.5 of probability to make a dumb action or a random action
+  """
+  if random.random() < 0.5:
+    return dumb_action(nim)
+  else: return random_action(nim)
+
+
+# Level 2: Medium-Advanced
 def random_action(nim: Nim):
   """
     The agent performs a random action
@@ -24,6 +47,82 @@ def random_action(nim: Nim):
   return Nimply(row, num_obj)
 
 
+# Level 3: Medium-Advanced
+def layered_action(nim: Nim):
+  """
+  Always takes the whole row's objs choosing randomly the row
+  """
+  row, num_obj = random.choice([(r, n) for r, n in enumerate(nim._rows) if n > 0])
+  if nim._k and num_obj > nim._k:
+    return Nimply(row, nim._k)
+  return Nimply(row, num_obj)
+
+
+# Level 4: DEMIGOD 
+def demigod_action(nim: Nim, prob_god=0.5):
+  """
+    There is a probability prob to play an expert move and a chance to play randomly
+  """
+  if random.random() < prob_god:
+    return expert_action(nim)
+  else: return random_action(nim)
+
+  
+# Level 5: GOD 
+def expert_action(nim: Nim):
+  """
+    The agent uses fixed rules based on nim-sum (expert-system)
+
+    Returns the index of the pile and the number of pieces removed as a Nimply namedtuple
+  """
+  board = nim._rows
+  k = nim._k
+
+  # Winning move if there is only one row left
+  tmp = [(i, r) for i, r in enumerate(board) if r > 0]
+  if len(tmp) == 1:
+    row, num_obj = tmp[0]
+    if not k or num_obj <= k:
+      return Nimply(row, num_obj) # Take the entire row
+
+
+  # Compute the nim-sum of all the heap sizes
+  x = nim_sum(board)
+
+  if x > 0:
+    # Current player on a insucure position -> is winning
+    # --> Has to generate a secure position (bad for the other player)
+    # --> Find a heap where the nim-sum of X and the heap-size is less than the heap-size.
+    # --> Then play on that heap, reducing the heap to the nim-sum of its original size with X
+    
+    good_rows = [] # A list is needed because of k
+    for row, row_size in enumerate(board):
+      if row_size == 0:
+        continue
+      ns = row_size ^ x # nim sum
+      if ns < row_size:
+        good_rows.append((row, row_size)) # This row will have nim sum = 0
+        
+    for row, row_size in good_rows:
+      board_tmp = deepcopy(board)
+      for i in range(row_size):
+       board_tmp[row] -= 1 
+       if nim_sum(board_tmp) == 0:  # winning move
+        num_obj = abs(board[row] - board_tmp[row])
+        if not k or num_obj <= k:
+          return Nimply(row, num_obj)
+  
+  # x == 0 or k force a bad move to the player
+  # Current player on a secure position or on a bad position bc of k -> is losing
+  # --> Can only generate an insicure position (good for the other player)
+  # --> Perform a random action bc it doesn't matter
+  return random_action(nim)
+  
+
+
+####################
+##  PLAY MATCHES  ##
+####################
 
 def evaluate(nim: Nim, n_matches=20, *, my_action, opponent_action=random_action, debug=False):
   """
@@ -74,3 +173,5 @@ def evaluate(nim: Nim, n_matches=20, *, my_action, opponent_action=random_action
       won += 1
       
   return won/n_matches
+
+
